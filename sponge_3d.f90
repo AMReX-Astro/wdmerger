df176ba48eed7dc3e3564c54ca42d407a8e8d378
Ann Almgren <asalmgren@lbl.gov>
2012-12-06 13:31:46 -0800

    domlo/domhi were being used as if they were problo/probhi.  We now define problo/probhi from domlo/domhi.

--- sponge_3d.f90
+++ sponge_3d.f90
@@ -7,35 +7,43 @@ subroutine sponge(uout,uout_l1,uout_l2,uout_l3,&
       
 
   implicit none
-  integer          :: lo(3),hi(3)
+  integer          :: lo(3),hi(3),domlo(3),domhi(3)
   integer          :: uout_l1,uout_l2,uout_l3,uout_h1,uout_h2,uout_h3
   double precision :: uout(uout_l1:uout_h1,uout_l2:uout_h2,&
        uout_l3:uout_h3,NVAR)
   double precision :: time, dt
-  double precision :: dx, dy, dz, domlo(3), domhi(3)
+  double precision :: dx, dy, dz
   
   double precision :: xx, yy, zz
   double precision :: xctr, yctr, zctr
   
   double precision :: ke_old, ke_new, sponge_mult, delta_sponge, radius, r_sponge, fac
+  double precision :: problo(3),probhi(3)
 
   integer :: i,j,k
 
-  xctr = HALF*(domlo(1) + domhi(1))
-  yctr = HALF*(domlo(2) + domhi(2))
-  zctr = HALF*(domlo(3) + domhi(3))
+  problo(1) =  domlo(1)   *dx
+  problo(2) =  domlo(2)   *dy
+  problo(3) =  domlo(3)   *dz
+  probhi(1) = (domhi(1)+1)*dx
+  probhi(2) = (domhi(2)+1)*dy
+  probhi(3) = (domhi(3)+1)*dz
+
+  xctr = HALF*(problo(1) + probhi(1))
+  yctr = HALF*(problo(2) + probhi(2))
+  zctr = HALF*(problo(3) + probhi(3))
   
-  r_sponge = 0.25*(domhi(1)-domlo(1))
-  delta_sponge = 0.025*(domhi(1)-domlo(1))
+  r_sponge = 0.25*(probhi(1)-problo(1))
+  delta_sponge = 0.025*(probhi(1)-problo(1))
   
   do k = lo(3),hi(3)
-     zz = domlo(3) + dble(k + HALF)*dz 
+     zz = problo(3) + dble(k + HALF)*dz 
      
      do j = lo(2),hi(2)
-        yy = domlo(2) + dble(j + HALF)*dy
+        yy = problo(2) + dble(j + HALF)*dy
         
         do i = lo(1),hi(1)
-           xx = domlo(1) + dble(i + HALF)*dx
+           xx = problo(1) + dble(i + HALF)*dx
            
            radius = sqrt((xx-xctr)**2 + (yy-yctr)**2 + (zz-zctr)**2)
            
