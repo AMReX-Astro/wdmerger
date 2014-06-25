34ff22cadd0bc4c48a91fa7fb7384de9cbd9c1c0
Michael Zingale <michael.zingale@stonybrook.edu>
2014-06-25 14:42:34 -0400

    tight_layout doesn't consider suptitle

--- scaling/edison-2014-06-24/scaling.py
+++ scaling/edison-2014-06-24/scaling.py
@@ -6,7 +6,8 @@ data = np.loadtxt("scaling.txt")
 cores = data[:,0]*data[:,1]
 time = data[:,2]
 
-pylab.loglog(cores, time, "o-", color="r")
+pylab.loglog(cores, time, "o-", color="r", 
+             label=r"3 levels, jumps of 2$\times$ (0 $\rightarrow$ 1) and 4$\times$ (1 $\rightarrow$ 2)")
 
 ideal = time[0]*cores[0]/cores
 
@@ -16,8 +17,9 @@ pylab.ylim(100,3000)
 
 pylab.xlabel("number of cores")
 pylab.ylabel("average time to advance the coarse level")
-pylab.suptitle("NERSC Edison Scaling for WD Merger", fontsize=16)
-pylab.title(r"3 levels, jumps of 2$\times$ (0 $\rightarrow$ 1) and 4$\times$ (1 $\rightarrow$ 2)", fontsize=14)
+pylab.title("NERSC Edison Scaling for WD Merger", fontsize=16)
+
+pylab.legend(frameon=False)
 
 pylab.tight_layout()
 
