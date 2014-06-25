767748f5a2bd014cb3830f21e54cb9046a437375
Michael Zingale <michael.zingale@stonybrook.edu>
2014-06-25 14:37:53 -0400

    tight layout

--- scaling/edison-2014-06-24/scaling.py
+++ scaling/edison-2014-06-24/scaling.py
@@ -16,6 +16,10 @@ pylab.ylim(100,3000)
 
 pylab.xlabel("number of cores")
 pylab.ylabel("average time to advance the coarse level")
-pylab.title("NERSC Edison Scaling for WD Merger (3 levels, jumps of 2x and 4x)")
+pylab.suptitle("NERSC Edison Scaling for WD Merger", fontsize=16)
+pylab.title(r"3 levels, jumps of 2$\times$ (0 $\rightarrow$ 1) and 4$\times$ (1 $\rightarrow$ 2)", fontsize=14)
+
+pylab.tight_layout()
 
 pylab.savefig("edison-wdmerger-scaling.png")
+pylab.savefig("edison-wdmerger-scaling.eps")
