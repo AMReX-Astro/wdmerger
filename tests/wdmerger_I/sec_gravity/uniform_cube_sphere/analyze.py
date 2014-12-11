ef1311c8538f900939106151f8dffd535a7f5d99
Max Katz <maximilian.katz@stonybrook.edu>
2014-12-11 02:00:44 -0500

    Updated the analysis script to calculate the convergence trend for the uniform sphere.

--- tests/wdmerger_I/sec_gravity/uniform_cube_sphere/analyze.py
+++ tests/wdmerger_I/sec_gravity/uniform_cube_sphere/analyze.py
@@ -1,86 +1,116 @@
 import yt
 import numpy as np
+import math
 from matplotlib import pyplot as plt
 
-ncell_arr = [16, 32, 64, 128, 256]
+ncell_arr = [16, 32, 64]
 
-l2 = np.zeros(len(ncell_arr))
+num_problems = 2
 
-index = 0
+l2 = np.zeros((num_problems,len(ncell_arr)))
 
-cube_a   = 2.0e0
-cube_rho = 1.0e3
+diameter = 2.0e0
+density  = 1.0e3
+
+radius   = diameter / 2.0
 
 Gconst   = 6.67428e-8
 
 c = np.zeros((2,3))
 
-for ncell in ncell_arr:
+for p in range(num_problems):
 
-    print "Now doing the comparison for ncell = ", ncell
+    problem = p + 1
 
-    dx = 3.2e0 / ncell
+    print "Now doing the analysis for problem", problem
 
-    # First we'll create the array for the analytical solution
+    index = 0
 
-    exact = yt.YTArray(np.zeros((ncell,ncell,ncell)), 'erg/g')
+    for ncell in ncell_arr:
 
-    for k in range(ncell):
-        zz = -1.6e0 + dx * (k + 0.5e0)
-        for j in range(ncell):
-            yy = -1.6e0 + dx * (j + 0.5e0)
-            for i in range(ncell):
-                xx = -1.6e0 + dx * (i + 0.5e0)
+        print "Now doing the comparison for ncell =", ncell
 
-                c[0,2] = -cube_a/2 - zz
-                c[1,2] =  cube_a/2 - zz
-                c[0,1] = -cube_a/2 - yy
-                c[1,1] =  cube_a/2 - yy
-                c[0,0] = -cube_a/2 - xx
-                c[1,0] =  cube_a/2 - xx
+        dx = 3.2e0 / ncell
 
-                phi = 0.0
-                
-                for ii in range(2):
-                    for jj in range(2):
-                        for ll in range(3):
+        # First we'll create the array for the analytical solution
 
-                            num1 = ( (c[ii,ll]**2 + c[jj,(ll+1) % 3]**2 + c[1,(ll+2) % 3]**2)**(0.5) + c[1,(ll+2) % 3] )**3
-                            num2 = ( (c[ii,ll]**2 + c[jj,(ll+1) % 3]**2 + c[0,(ll+2) % 3]**2)**(0.5) - c[0,(ll+2) % 3] )
-                            den1 = ( (c[ii,ll]**2 + c[jj,(ll+1) % 3]**2 + c[1,(ll+2) % 3]**2)**(0.5) - c[1,(ll+2) % 3] )
-                            den2 = ( (c[ii,ll]**2 + c[jj,(ll+1) % 3]**2 + c[0,(ll+2) % 3]**2)**(0.5) + c[0,(ll+2) % 3] )**3
+        exact = yt.YTArray(np.zeros((ncell,ncell,ncell)), 'erg/g')
 
-                            phi = phi + 0.5 * (-1)**(ii+jj) * ( c[ii,ll] * c[jj,(ll+1) % 3] * \
-                                        np.log( num1 * num2 / (den1 * den2) ) )
+        for k in range(ncell):
+            zz = -1.6e0 + dx * (k + 0.5e0)
+            for j in range(ncell):
+                yy = -1.6e0 + dx * (j + 0.5e0)
+                for i in range(ncell):
+                    xx = -1.6e0 + dx * (i + 0.5e0)
 
-                for ii in range(2):
-                    for jj in range(2):
-                        for kk in range(2):
-                            for ll in range(3):
-                                phi = phi + (-1)**(ii+jj+kk+1) * c[ii,ll]**2 * \
-                                      np.arctan2( c[ii,ll] * c[kk,(ll+2) % 3], \
-                                                  c[ii,ll]**2 + c[jj,(ll+1) % 3]**2 + c[jj,(ll+1) % 3] * \
-                                                  (c[ii,ll]**2 + c[jj,(ll+1) % 3]**2 + c[kk,(ll+2) % 3]**2)**(0.5) )
+                    phi = 0.0
 
+                    if (problem == 1):
 
-                exact[i,j,k] = phi * 0.5 * Gconst * cube_rho
+                        c[0,2] = -radius - zz
+                        c[1,2] =  radius - zz
+                        c[0,1] = -radius - yy
+                        c[1,1] =  radius - yy
+                        c[0,0] = -radius - xx
+                        c[1,0] =  radius - xx
 
-    exact_L2 = np.sqrt(dx**3 * (exact**2).sum())
+                        for ii in range(2):
+                            for jj in range(2):
+                                for ll in range(3):
 
-    pf_name = "results/" + str(ncell) + "/plt00000"
+                                    num1 = ( (c[ii,ll]**2 + c[jj,(ll+1) % 3]**2 + c[1,(ll+2) % 3]**2)**(0.5) + c[1,(ll+2) % 3] )**3
+                                    num2 = ( (c[ii,ll]**2 + c[jj,(ll+1) % 3]**2 + c[0,(ll+2) % 3]**2)**(0.5) - c[0,(ll+2) % 3] )
+                                    den1 = ( (c[ii,ll]**2 + c[jj,(ll+1) % 3]**2 + c[1,(ll+2) % 3]**2)**(0.5) - c[1,(ll+2) % 3] )
+                                    den2 = ( (c[ii,ll]**2 + c[jj,(ll+1) % 3]**2 + c[0,(ll+2) % 3]**2)**(0.5) + c[0,(ll+2) % 3] )**3
 
-    pf = yt.load(pf_name)
+                                    phi = phi + 0.5 * (-1)**(ii+jj) * ( c[ii,ll] * c[jj,(ll+1) % 3] * \
+                                                np.log( num1 * num2 / (den1 * den2) ) )
 
-    plot_data = pf.covering_grid(level=0,left_edge=[-1.6,-1.6,-1.6], dims=pf.domain_dimensions)['phiGrav']
+                        for ii in range(2):
+                            for jj in range(2):
+                                for kk in range(2):
+                                    for ll in range(3):
+                                        phi = phi + (-1)**(ii+jj+kk+1) * c[ii,ll]**2 * \
+                                              np.arctan2( c[ii,ll] * c[kk,(ll+2) % 3], \
+                                                          c[ii,ll]**2 + c[jj,(ll+1) % 3]**2 + c[jj,(ll+1) % 3] * \
+                                                          (c[ii,ll]**2 + c[jj,(ll+1) % 3]**2 + c[kk,(ll+2) % 3]**2)**(0.5) )
 
-    L2_field = (plot_data - exact)**2
+                        exact[i,j,k] = phi * 0.5 * Gconst * density
 
-    l2[index] = np.sqrt(dx**3 * L2_field.sum())  / exact_L2
+                    elif (problem == 2):
 
-    index += 1
+                        rr = (xx**2 + yy**2 + zz**2)**0.5
 
-plt.plot(ncell_arr,l2)
+                        mass = 4.0 / 3.0 * math.pi * radius**3 * density
+
+                        if (rr <= radius):
+                            exact[i,j,k] = Gconst * mass * (3 * radius**2 - rr**2) / (2 * radius**3)
+                        else:
+                            exact[i,j,k] = Gconst * mass / rr
+
+                    else:
+                        
+                        print "This is not a valid problem."
+
+        exact_L2 = np.sqrt(dx**3 * (exact**2).sum())
+
+        pf_name = "results/problem" + str(problem) + "/" + str(ncell) + "/plt00000"
+
+        pf = yt.load(pf_name)
+
+        plot_data = pf.covering_grid(level=0,left_edge=[-1.6,-1.6,-1.6], dims=pf.domain_dimensions)['phiGrav']
+
+        L2_field = (plot_data - exact)**2
+
+        l2[p,index] = np.sqrt(dx**3 * L2_field.sum())  / exact_L2
+
+        index += 1
+
+print l2
+
+plt.plot(ncell_arr,l2[0,:],ncell_arr,l2[1,:])
 plt.xlabel("Number of cells per dimension")
 plt.ylabel("Relative L2 error")
+plt.xscale('log')
 plt.yscale('log')
 plt.savefig('phi_comparison.png')
--- tests/wdmerger_I/sec_gravity/uniform_cube_sphere/run_test.sh
+++ tests/wdmerger_I/sec_gravity/uniform_cube_sphere/run_test.sh
@@ -23,6 +23,8 @@ function move_results {
 
 }
 
+# Check if results directory already exists, and if not then create it.
+
 results_dir=results
 
 if [ ! -d $results_dir ]; then
@@ -31,14 +33,24 @@ fi
 
 # Loop over the resolutions in question
 
-for ncell in 16 32 64 128 256
+for problem in 1 2
 do
-  dir=$results_dir/$ncell
-  if [ ! -d $dir ]; then
-    mkdir $dir
-    echo "Now doing ncell =" $ncell
-    sed -i "/amr.n_cell/c amr.n_cell = $ncell $ncell $ncell" $inputs
-    $exec $Castro $inputs > info.out
-    move_results $dir
+  echo "Now doing problem =" $problem
+  sed -i "/problem/c problem = $problem" $probin
+
+  if [ ! -d $results_dir/problem$problem ]; then
+      mkdir $results_dir/problem$problem
   fi
+
+  for ncell in 16 32 64
+  do
+    dir=$results_dir/problem$problem/$ncell
+    if [ ! -d $dir ]; then
+      mkdir $dir
+      echo "Now doing ncell =" $ncell
+      sed -i "/amr.n_cell/c amr.n_cell = $ncell $ncell $ncell" $inputs
+      $exec $Castro $inputs > info.out
+      move_results $dir
+    fi
+  done
 done
