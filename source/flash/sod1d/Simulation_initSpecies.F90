3a2d616696ae4440cd89952ad263f34a735ea2a1
Max Katz <maximilian.katz@stonybrook.edu>
2015-02-16 22:25:45 -0600

    Remove the FLASH source code since we're not actively maintaining it. We can always restore it later if there is interest in doing FLASH comparisons.

--- source/flash/sod1d/Simulation_initSpecies.F90
+++
@@ -1,9 +0,0 @@
-subroutine Simulation_initSpecies()
-implicit none
-#include "Multispecies.h"
-#include "Flash.h"
-
-call Multispecies_setProperty(C12_SPEC, A, 12.)
-call Multispecies_setProperty(C12_SPEC, Z, 6.)
-
-end subroutine Simulation_initSpecies
