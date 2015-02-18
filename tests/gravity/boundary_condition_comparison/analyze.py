2a71fd659763ae02b5be822638cc58665a9a5dd9
Max Katz <maximilian.katz@stonybrook.edu>
2015-02-18 00:52:12 -0600

    Updates to analysis of boundary_condition_comparison

--- tests/gravity/boundary_condition_comparison/analyze.py
+++ tests/gravity/boundary_condition_comparison/analyze.py
@@ -1,28 +1,54 @@
 import yt
 import numpy as np
+import matplotlib
 from matplotlib import pyplot as plt
 import wdmerger
 
-ell = range(21)
+# Determine our range of multiple orders by reading in the run_test.sh file
+
+minell = None
+maxell = None
+
+file = open('run_test.sh', 'r')
+for line in file:
+    sline = line.split('=')
+    if (len(sline) > 1):
+        if (sline[0] == 'maxell'):
+            maxell = int(sline[1])
+        elif (sline[0] == 'minell'):
+            minell = int(sline[1])
+
+# Safety check
+
+if (minell == None):
+    print "Could not find minimum l value in run_test.sh; exiting."
+    exit()
+
+if (maxell == None):
+    print "Could not find maximum l value in run_test.sh; exiting."
+    exit()
+
+ell = range(minell, maxell+1)
 
 l2 = np.zeros(len(ell))
 
+plt_name = "plt00000"
+
 # Load exact results, for comparison
 
-pf_exact = yt.load("results/true/plt00000")
-print pf_exact.domain_dimensions
-exact_data = pf_exact.covering_grid(level=0, left_edge=[0.0,0.0,0.0], dims=pf_exact.domain_dimensions)['phiGrav']
+pf_exact = yt.load("results/true/output/" + plt_name)
+exact_data = pf_exact.covering_grid(level=0, left_edge=pf_exact.domain_left_edge, dims=pf_exact.domain_dimensions)['phiGrav']
 exact_L2 = np.sqrt((exact_data**2).sum())
 
 index = 0
 
 for l in ell:
 
-    pf_name = "results/" + str(l) + "/plt00000"
+    pf_name = "results/" + str(l) + "/output/" + plt_name
 
     pf = yt.load(pf_name)
 
-    plot_data = pf.covering_grid(level=0,left_edge=[0.0,0.0,0.0], dims=pf.domain_dimensions)['phiGrav']                                           
+    plot_data = pf.covering_grid(level=0,left_edge=pf_exact.domain_left_edge, dims=pf.domain_dimensions)['phiGrav']                                           
 
     L2_field = (plot_data - exact_data)**2
 
@@ -32,9 +58,10 @@ for l in ell:
 
 eps_filename = 'plots/bc_comparison.eps'
 
-plt.plot(ell,l2)
-plt.xlabel("Maximum multipole order")
-plt.ylabel("Relative L2 error")
+plt.plot(ell,l2, ls='-', lw=4.0, marker='o', ms=12.0)
+plt.xlabel("Maximum multipole order", fontsize=20)
+plt.ylabel(r"Relative $L^2$ error", fontsize=20)
+plt.tick_params(labelsize=16)
 plt.yscale('log')
 plt.savefig(eps_filename)
 wdmerger.insert_commits_into_eps(eps_filename, pf_name, 'plot')
--- tests/gravity/boundary_condition_comparison/run_test.sh
+++ tests/gravity/boundary_condition_comparison/run_test.sh
@@ -1,18 +1,27 @@
 source $WDMERGER_HOME/job_scripts/run_utils.sh
 
+minell=0
+maxell=20
+
+# Loop over the multipole orders we want to examine
+
 if [ $MACHINE == "BLUE_WATERS" ]; then
     nprocs=16
     walltime=1:00:00
 fi
 
-# Loop over the multipole orders we want to examine
-
-for l in {0..20}
+for l in $(seq $minell $maxell)
 do
   dir=$results_dir/$l
+  sed -i "/gravity.direct_sum_bcs/c gravity.direct_sum_bcs = 0" $compile_dir/$inputs
   sed -i "/gravity.max_multipole_order/c gravity.max_multipole_order = $l" $compile_dir/$inputs
   run $dir $nprocs $walltime
 done
+
+if [ $MACHINE == "BLUE_WATERS" ]; then
+    nprocs=512
+    walltime=1:00:00
+fi
 
 # Now do the 'exact' direct summation, for comparison purposes
 
