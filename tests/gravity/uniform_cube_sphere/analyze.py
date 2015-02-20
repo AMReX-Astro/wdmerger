69a4da98e97dc1c103855254a63af0550374db44
Max Katz <maximilian.katz@stonybrook.edu>
2015-02-19 20:07:09 -0600

    Updates for the uniform cube/sphere test.

--- tests/gravity/uniform_cube_sphere/analyze.py
+++ tests/gravity/uniform_cube_sphere/analyze.py
@@ -8,15 +8,73 @@ ncell_arr = [16, 32, 64, 128, 256]
 num_problems = 3
 
 l2 = np.zeros((num_problems,len(ncell_arr)))
+convergence = np.zeros((num_problems,len(ncell_arr)))
 
-diameter = yt.YTQuantity(2.0e0, 'cm')
-density  = yt.YTQuantity(1.0e3, 'g/cm**3')
+# Get the problem setup data from the probin file
+
+probin_file  = open('source/probin', 'r')
+probin_lines = probin_file.readlines()
+
+diameter = None
+density = None
+ambient_density = None
+
+for line in probin_lines:
+    lsplit = line.split()
+    if (len(lsplit) >= 3):
+        if (lsplit[0] == 'diameter'):
+            diameter = lsplit[2]
+            diameter = float(diameter.replace("d","e"))
+            diameter = yt.YTQuantity(diameter, 'cm')
+        elif (lsplit[0] == 'density'):
+            density = lsplit[2]
+            density = float(density.replace("d","e"))
+            density = yt.YTQuantity(density, 'g/cm**3')
+        elif (lsplit[0] == 'ambient_dens'):
+            ambient_density = lsplit[2]
+            ambient_density = float(ambient_density.replace("d","e"))
+            ambient_density = yt.YTQuantity(ambient_density, 'g/cm**3')
+
+if (diameter == None):
+    print "Density not found in the probin file; exiting."
+    exit()
+
+if (density == None):
+    print "Diameter not found in the probin file; exiting."
+    exit()
+
+if (ambient_density == None):
+    print "Ambient density not found in the probin file; exiting."
+    exit()
+
+probin_file.close()
 
 radius   = diameter / 2.0e0
 
-ambient_density = yt.YTQuantity(1.0e-8, 'g/cm**3')
+# Get the gravitational constant from the CASTRO cgs constants file
 
-Gconst   = yt.YTQuantity(6.67428e-8, 'cm**3*g**-1*s**-2')
+CASTRO_DIR = wdmerger.get_castro_dir()
+
+units_file  = open(CASTRO_DIR + '/constants/constants_cgs.f90')
+units_lines = units_file.readlines()
+
+Gconst = None
+
+for line in units_lines:
+    lsplit = line.split()
+    if (len(lsplit) >= 4):
+        if (lsplit[3] == 'Gconst'):
+            Gconst = lsplit[5]
+            # Remove the kind specification at the end of the string
+            Gconst = Gconst[:-5]
+            Gconst = float(Gconst.replace("d","e"))
+            Gconst = yt.YTQuantity(Gconst, 'cm**3*g**-1*s**-2')
+
+if (Gconst == None):
+    print "Gravitational constant not found in CGS constants file; exiting."
+    exit()
+
+units_file.close()
 
 for p in range(num_problems):
 
@@ -32,7 +90,7 @@ for p in range(num_problems):
 
         # First we load in the numerical data
 
-        pf_name = "results/problem" + str(problem) + "/" + str(ncell) + "/plt00000"
+        pf_name = "results/p" + str(problem) + "/n" + str(ncell) + "/output/plt00000"
 
         pf = yt.load(pf_name)
 
@@ -120,24 +178,59 @@ for p in range(num_problems):
 
         l2[p,index] = np.sqrt(dx[0]*dx[1]*dx[2] * L2_field.sum())  / exact_L2
 
+        if (index > 0):
+            convergence[p,index] = np.log(l2[p,index-1] / l2[p,index]) / np.log(ncell_arr[index] / ncell_arr[index-1])
+
         index += 1
 
-    plt.plot(ncell_arr,l2[p,:],label=title)
+    if (p == 0):
+        lstyle = '--'
+        mrkr = 'o'
+    elif (p == 1):
+        lstyle = '-.'
+        mrkr = 'o'
+    elif (p == 2):
+        lstyle = ':'
+        mrkr = 's'
 
-# Set up an analytical curve that represents perfect second-order convergence
+    plt.plot(ncell_arr,l2[p,:], label=title, linestyle=lstyle, lw = 4.0, marker=mrkr, ms = 12.0)
 
-maxerr = max(l2[:,0])
-second_order = 3.0 * maxerr * (np.array(ncell_arr) / ncell_arr[0])**(-2.0)
+# Write out the data to a text file.
+
+file = open('plots/phi_comparison.dat','w')
+
+file.write('     Problem 1          Problem 2          Problem 3   \n')
+file.write('  Error    Order     Error    Order     Error    Order \n')
+
+for n in range(len(ncell_arr)):
+    outString = ''
+    for p in range(num_problems):
+        fmt1 = '{:4.3e}'
+        fmt2 = '{: 4.3f}'
+        outString += fmt1.format(l2[p,n]) + ' ' + fmt2.format(convergence[p,n]) + '   '
+
+    file.write(outString)
+    file.write('\n')
+
+file.close()
+
+# Set up an analytical curve that represents perfect second-order convergence.
+# We'll place it a bit below the lowest curve on the grid, and give it a grayscale
+# color by using the color = 'float' command where float is some number between 0 and 1.
+
+minerr = min(l2[:,0])
+second_order = (minerr / 3.0) * (np.array(ncell_arr) / ncell_arr[0])**(-2.0)
+
+plt.plot(ncell_arr, second_order, label='Second order convergence', linestyle='-', lw = 3.0, color='0.75')
 
 eps_filename = 'plots/phi_comparison.eps'
 
-plt.plot(ncell_arr, second_order, label='Second order convergence')
-
 plt.xlim([ncell_arr[0], ncell_arr[len(ncell_arr)-1]])
-plt.xlabel("Number of cells per dimension")
-plt.ylabel(r"Relative L$^2$ error")
+plt.xlabel("Number of cells per dimension", fontsize=20)
+plt.ylabel(r"Relative L$^2$ error", fontsize=20)
 plt.xscale('log', basex=2)
 plt.yscale('log')
-plt.legend(loc='upper right')
+plt.legend(loc='upper right', fontsize=12, handlelength=5)
+plt.tick_params(labelsize=16)
 plt.savefig(eps_filename)
 wdmerger.insert_commits_into_eps(eps_filename, pf_name, 'plot')
--- tests/gravity/uniform_cube_sphere/run_test.sh
+++ tests/gravity/uniform_cube_sphere/run_test.sh
@@ -8,13 +8,27 @@ do
   for ncell in 16 32 64 128 256
   do
 
-    dir=$results_dir/problem$problem/n$ncell
+    dir=$results_dir/p$problem/n$ncell
     sed -i "/problem/c problem = $problem" $compile_dir/$probin
     sed -i "/amr.n_cell/c amr.n_cell = $ncell $ncell $ncell" $compile_dir/$inputs
 
     if [ $MACHINE == "BLUE_WATERS" ]; then
-	nprocs=16
-	walltime=1:00:00
+	if   [ $ncell -eq 16  ]; then
+	    nprocs=1
+	    walltime=1:00:00
+	elif [ $ncell -eq 32  ]; then
+	    nprocs=8
+	    walltime=1:00:00
+	elif [ $ncell -eq 64  ]; then
+	    nprocs=16
+	    walltime=1:00:00
+	elif [ $ncell -eq 128 ]; then
+	    nprocs=64
+	    walltime=1:00:00
+	elif [ $ncell -eq 256 ]; then
+	    nprocs=512
+	    walltime=1:00:00
+	fi
     fi
 
     run $dir $nprocs $walltime
