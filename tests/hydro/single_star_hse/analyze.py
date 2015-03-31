e6be013eb846170d6006ce972f5822959ffe9ac0
Max Katz <maximilian.katz@stonybrook.edu>
2015-03-31 10:58:13 -0500

    Updates for the single star test, including the static and moving cases.

--- tests/hydro/single_star_hse/analyze.py
+++ tests/hydro/single_star_hse/analyze.py
@@ -2,37 +2,127 @@ import yt
 import numpy as np
 from matplotlib import pyplot as plt
 import wdmerger
+import os
 
-# Set the name of the diagnostic output file
+# Problem parameters
 
-ncell = 64
+ncell_arr = [ 256, 512, 1024 ]
+vel_arr   = [ 'motion', 'static' ]
 
-diag_filename = "results/" + str(ncell) + "/single_star.out"
-
-# Obtain the time column, and the spherical radii denoting different density cutoffs
-
-time = wdmerger.get_column('TIME', diag_filename)
+linestyles = ["-", "--","-.",":"]
 
 nCols = 7
 
-rad = np.zeros((len(time), nCols))
+for vel in vel_arr:
 
-for n in range(nCols):
-    rad[:,n] = wdmerger.get_column('1E' + str(n) + ' BOUNDARY RAD.')
+    for c in range(nCols):
 
-    # Divide radii by initial radii
+        # Set the name of the diagnostic output file
 
-    rad[:,n] = rad[:,n] / rad[0,n]
+        eps_filename = "plots/single_star_" + str(vel) + "_1e" + str(c) + "_radius.eps"
 
-eps_filename = "plots/single_star_hse.eps"
+        if (os.path.isfile(eps_filename)):
+            print "Plot with filename " + eps_filename + " already exists; skipping."
+            continue        
 
-# Let's plot with reversed axis for aesthetic purposes
+        print "Generating plot with filename " + eps_filename
 
-plt.plot(time, rad[:,2], label=r'$\rho=10^2$')
-plt.plot(time, rad[:,4], label=r'$\rho=10^4$')
-plt.plot(time, rad[:,6], label=r'$\rho=10^6$')
-plt.legend(loc='lower right')
-plt.xlabel("Time (s)")
-plt.ylabel("Radius / Initial Radius")
-plt.savefig(eps_filename)
-wdmerger.insert_commits_into_eps(eps_filename, diag_filename, 'diag')
+        idx = 0
+
+        for n in ncell_arr:
+
+            # Account for the fact that we run at higher resolution when we're in the moving frame.
+
+            if ( vel == 'motion' ):
+                n = n * 4
+
+            diag_filename = "results/" + str(vel) + "/n" + str(n) + "/output/star_diag.out"
+
+            timeCol = wdmerger.get_column('TIME', diag_filename)
+
+            # Obtain the spherical radii denoting different density cutoffs
+
+            radCol = wdmerger.get_column('PRIMARY 1E' + str(c) + ' RADIUS', diag_filename)
+
+            # Divide radii by initial radii, then calculate difference with respect to
+            # the initial radius.
+
+            radCol /= radCol[0]
+
+            plt.plot(timeCol, radCol, linestyle=linestyles[idx], linewidth=4.0, label=r'n = ' + str(n))
+
+            idx += 1
+
+        plt.legend(loc='upper center',ncol=len(ncell_arr))
+        plt.xlabel("Time (s)", fontsize=20)
+        plt.ylabel("Radius / Initial Radius", fontsize=20)
+
+        if ( vel == 'motion' ):
+            ylim_l = 0.95
+            ylim_r = 2.05
+        elif ( vel == 'static' ):
+            ylim_l = 0.995
+            ylim_r = 1.1
+
+        plt.axis([timeCol[0], timeCol[-1], ylim_l, ylim_r])
+        plt.tick_params(labelsize=16)
+
+        plt.savefig(eps_filename)
+        wdmerger.insert_commits_into_eps(eps_filename, diag_filename, 'diag')
+
+        plt.clf()
+
+# Now generate plots that compare the reference frames
+
+for c in range(nCols):
+
+    eps_filename = "plots/single_star_compare_1e" + str(c) + "_radius.eps"
+
+    if (os.path.isfile(eps_filename)):
+        print "Plot with filename " + eps_filename + " already exists; skipping."
+        continue        
+
+    print "Generating plot with filename " + eps_filename
+
+    idx = 0
+
+    for n in ncell_arr:
+
+        for vel in vel_arr:
+
+            # Account for the fact that we run at higher resolution when we're in the moving frame.
+
+            if ( vel == 'motion' ):
+                nc = n * 4
+            elif ( vel == 'static' ):
+                nc = n
+
+            # First do the static case
+
+            diag_filename = "results/" + str(vel) + "/n" + str(nc) + "/output/star_diag.out"
+
+            timeCol = wdmerger.get_column('TIME', diag_filename)
+
+            # Obtain the spherical radii denoting different density cutoffs
+
+            radCol = wdmerger.get_column('PRIMARY 1E' + str(c) + ' RADIUS', diag_filename)
+
+            # Divide radii by initial radii, then calculate difference with respect to
+            # the initial radius.
+
+            radCol /= radCol[0]
+
+            plt.plot(timeCol, radCol, linestyle=linestyles[idx], linewidth=4.0, label=str(vel) + ", " + str(nc))
+
+        idx += 1
+
+    plt.legend(loc='upper center',ncol=len(ncell_arr),columnspacing=0.5)
+    plt.xlabel("Time (s)", fontsize=20)
+    plt.ylabel("Radius / Initial Radius", fontsize=20)
+    plt.axis([timeCol[0], timeCol[-1], 0.95, 2.05])
+    plt.tick_params(labelsize=16)
+
+    plt.savefig(eps_filename)
+    wdmerger.insert_commits_into_eps(eps_filename, diag_filename, 'diag')
+
+    plt.clf()
--- tests/hydro/single_star_hse/run_test.sh
+++ tests/hydro/single_star_hse/run_test.sh
@@ -5,27 +5,122 @@ source $WDMERGER_HOME/job_scripts/run_utils.sh
 mass_P=" 0.90"
 mass_S="-1.00"
 
-# Loop over resolutions
+stop_time=200.0
+castro_do_rotation=0
 
-for ncell in 64 128 256
+amr_check_per=20.0
+amr_plot_per=20.0
+
+# Loop over the resolutions for the stationary case
+
+for ncell in 256 512 1024
 do
-  dir=$results_dir/n$ncell
-  amr_n_cell="$ncell $ncell $ncell"
+  dir=$results_dir/static/n$ncell
+
+  if [ $ncell -eq 512 ]; then
+      amr_n_cell="256 256 256"
+      amr_ref_ratio="2"
+      amr_max_level="1"
+  elif [ $ncell -eq 1024 ]; then
+      amr_n_cell="256 256 256"
+      amr_ref_ratio="4"
+      amr_max_level="1"
+      amr_max_grid_size="32"
+  elif [ $ncell -eq 2048 ]; then
+      amr_n_cell="256 256 256"
+      amr_ref_ratio="4 2"
+      amr_max_level="2"
+      amr_max_grid_size="32 32 48"
+  else
+      amr_n_cell="$ncell $ncell $ncell"
+  fi
 
   if [ $MACHINE == "BLUE_WATERS" ]; then
 
-    if [ $ncell -eq 32 ]; then
-	nprocs=32
-	walltime=1:00:00
-    elif [ $ncell -eq 64 ]; then
-	nprocs=32
-	walltime=12:00:00
-    elif [ $ncell -eq 128 ]; then
-	nprocs=128
-	walltime=24:00:00
-    fi
+      if   [ $ncell -eq 128 ]; then
+	  nprocs=64
+	  walltime=12:00:00
+      elif [ $ncell -eq 256 ]; then
+	  nprocs=512
+	  walltime=24:00:00
+      elif [ $ncell -eq 512 ]; then
+	  nprocs=512
+	  walltime=24:00:00
+      elif [ $ncell -eq 1024 ]; then
+	  nprocs=512
+	  walltime=24:00:00
+      elif [ $ncell -eq 2048 ]; then
+	  nprocs=256
+	  walltime=24:00:00
+      fi
 
   fi
 
   run $dir $nprocs $walltime
+done
+
+
+
+
+# Now we'll do the case where the star moves.
+# We want the star to evolve for the same amount
+# of time as the previous simulations. One way to
+# do this is to start the star in the bottom left
+# corner and have it move toward the top right.
+# We widen the grid and use the AMR to zoom in
+# so that it has the same effective resolution
+# as the 256**3 run.
+
+geometry_prob_lo="-2.048e10 -2.048e10 -2.048e10"
+geometry_prob_hi=" 2.048e10  2.048e10  2.048e10"
+
+center_fracx=0.125d0
+center_fracy=0.125d0
+center_fracz=0.125d0
+
+bulk_velocity=256000000
+
+bulk_velx=$(echo "$bulk_velocity / sqrt(3)" | bc -l)
+bulk_vely=$(echo "$bulk_velocity / sqrt(3)" | bc -l)
+bulk_velz=$(echo "$bulk_velocity / sqrt(3)" | bc -l)
+
+for ncell in 1024 2048 4096
+do
+
+  if [ $ncell -eq 1024 ]; then
+    amr_n_cell="256 256 256"
+    amr_ref_ratio="4"
+    amr_max_level="1"
+    amr_max_grid_size="32 32"
+  elif [ $ncell -eq 2048 ]; then
+      amr_n_cell="256 256 256"
+      amr_ref_ratio="4 2"
+      amr_max_level="2"
+      amr_max_grid_size="32 32 32"
+  elif [ $ncell -eq 4096 ]; then
+      amr_n_cell="256 256 256"
+      amr_ref_ratio="4 4"
+      amr_max_level="2"
+      amr_max_grid_size="32 32 48"
+  fi
+
+  dir=$results_dir/motion/n$ncell
+
+  if [ $MACHINE == "BLUE_WATERS" ]; then
+
+      if [ $ncell -eq 1024 ]; then
+	  nprocs=512
+	  walltime=24:00:00
+      elif [ $ncell -eq 2048 ]; then
+	  nprocs=512
+	  walltime=24:00:00
+      elif [ $ncell -eq 4096 ]; then
+	  nprocs=512
+	  walltime=24:00:00
+      fi
+
+  fi
+
+  run $dir $nprocs $walltime
+
 done
