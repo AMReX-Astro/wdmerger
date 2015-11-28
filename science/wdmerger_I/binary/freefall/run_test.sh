0e5aad1409dc7b37b543a70ea588681b6a4bd897
Max Katz <maximilian.katz@stonybrook.edu>
2015-11-28 01:39:51 -0500

    Updated freefall test script

--- science/wdmerger_I/binary/freefall/run_test.sh
+++ science/wdmerger_I/binary/freefall/run_test.sh
@@ -3,7 +3,7 @@ source $WDMERGER_HOME/job_scripts/run_utils.sh
 # Problem-specific variables
 
 mass_P=0.90
-mass_S=0.81
+mass_S=0.75
 
 castro_do_rotation=0
 castro_rotational_period=100.0
@@ -13,35 +13,28 @@ no_orbital_kick=T
 amr_plot_per=1.0
 amr_check_per=1.0
 
+castro_do_react=0
+
 # We can work out the stopping time using the formula
 # t_freefall = rotational_period / (4 * sqrt(2)).
 # We'll stop 90% of the way there because that's about
 # when the stars start coming into contact, and the 
 # assumption of spherically symmetric stars breaks down.
 
-stop_time=$(echo "0.90 * $(rotational_period) / (4.0 * sqrt(2))" | bc -l)
+stop_time=$(echo "0.90 * $castro_rotational_period / (4.0 * sqrt(2))" | bc -l)
 
-# Loop over the resolutions in question
+dir=$results_dir
 
-for ncell in 64 128 256
-do
-  dir=$results_dir/n$ncell
-  amr_n_cell="$ncell $ncell $ncell"
+if [ $MACHINE == "BLUE_WATERS" ]; then
 
-  if [ $MACHINE == "BLUE_WATERS" ]; then
+  nprocs="2048"
+  walltime="2:00:00"
 
-    if   [ $ncell -eq 64  ]; then
-	nprocs="64"
-	walltime="2:00:00"
-    elif [ $ncell -eq 128 ]; then
-	nprocs="256"
-	walltime="2:00:00"
-    elif [ $ncell -eq 256 ]; then
-	nprocs="2048"
-	walltime="2:00:00"
-    fi
+elif [ $MACHINE == "LIRED" ]; then
 
-  fi
+  nprocs="144"
+  walltime="12:00:00"
 
-  run $dir $nprocs $walltime
-done
+fi
+
+run
