781b657a6695463ba1c20b896633c7819bf71aa1
Max Katz <maximilian.katz@stonybrook.edu>
2016-02-09 14:42:37 -0500

    The test to de-tag for refinement near boundaries was clearing tags near the symmetric boundary in 2D axisymmetric also; this is now fixed.

--- source/problem_tagging_nd.f90
+++ source/problem_tagging_nd.f90
@@ -14,7 +14,7 @@ contains
 
     use bl_constants_module, only: ZERO, HALF, TWO
     use meth_params_module, only: NVAR, URHO, UTEMP
-    use prob_params_module, only: center, probhi
+    use prob_params_module, only: center, probhi, dim, Symmetry, physbc_lo, physbc_hi
     use probdata_module, only: max_tagging_radius, &
                                max_stellar_tagging_level, &
                                max_temperature_tagging_level, &
@@ -35,17 +35,19 @@ contains
     double precision :: problo(3),dx(3),time
     integer          :: level,set,clear
 
-    integer          :: i, j, k
-    double precision :: x,y,z,r,r_P,r_S
+    integer          :: i, j, k, n
+    double precision :: loc(3), r, r_P, r_S
+
+    logical          :: outer_boundary_test(3)
 
     do k = lo(3), hi(3)
-       z = problo(3) + (dble(k) + HALF)*dx(3)
+       loc(3) = problo(3) + (dble(k) + HALF) * dx(3)
 
        do j = lo(2), hi(2)
-          y = problo(2) + (dble(j) + HALF)*dx(2)
+          loc(2) = problo(2) + (dble(j) + HALF) * dx(2)
 
           do i = lo(1), hi(1)
-             x = problo(1) + (dble(i) + HALF)*dx(1)
+             loc(1) = problo(1) + (dble(i) + HALF) * dx(1)
 
              if (level < max_stellar_tagging_level) then
 
@@ -56,8 +58,8 @@ contains
                    ! radius to ensure there aren't any sharp gradients in regions of
                    ! greater than ambient density.
 
-                   r_P = ( (x-com_P(1))**2 + (y-com_P(2))**2 + (z-com_P(3))**2 )**HALF
-                   r_S = ( (x-com_S(1))**2 + (y-com_S(2))**2 + (z-com_S(3))**2 )**HALF
+                   r_P = ( sum((loc-com_P)**2) )**HALF
+                   r_S = ( sum((loc-com_S)**2) )**HALF
 
                    if (r_P <= roche_tagging_factor * roche_rad_P) then
                       tag(i,j,k) = set
@@ -96,21 +98,37 @@ contains
 
              ! Clear all tagging that occurs outside the radius set by max_tagging_radius.
 
-             r = ((x-center(1))**2 + (y-center(2))**2 + (z-center(3))**2)**HALF
+             r = ( sum((loc-center)**2) )**HALF
 
              if (r .gt. max_tagging_radius * maxval(abs(problo-center)) .or. &
-                  r .gt. max_tagging_radius * maxval(abs(probhi-center)) ) then
+                 r .gt. max_tagging_radius * maxval(abs(probhi-center)) ) then
 
                 tag(i,j,k) = clear
 
              endif
 
-             ! We must ensure that the outermost zones are untagged due to the Poisson equation boundary conditions.
-             ! This is necessary in addition to the above step because the problem center doesn't always coincide with the geometric center.
+             ! We must ensure that the outermost zones are untagged
+             ! due to the Poisson equation boundary conditions.
+             ! This is necessary in addition to the above step
+             ! because the problem center doesn't always coincide
+             ! with the geometric center. However we can avoid this
+             ! restriction on symmetric boundaries.
 
-             if (x .lt. problo(1) + TWO * dx(1) .or. x .gt. probhi(1) - TWO * dx(1) .or. &
-                  y .lt. problo(2) + TWO * dx(2) .or. y .gt. probhi(2) - TWO * dx(2) .or. &
-                  z .lt. problo(3) + TWO * dx(3) .or. z .gt. probhi(3) - TWO * dx(3)) then
+             outer_boundary_test = .false.
+
+             do n = 1, dim
+
+                if ((physbc_lo(n) .ne. Symmetry) .and. (loc(n) .lt. problo(n) + TWO * dx(1))) then
+                   outer_boundary_test = .true.
+                endif
+
+                if ((physbc_hi(n) .ne. Symmetry) .and. (loc(n) .gt. probhi(n) - TWO * dx(1))) then
+                   outer_boundary_test = .true.
+                endif
+
+             enddo
+
+             if ( any(outer_boundary_test) ) then
 
                 tag(i,j,k) = clear
 
